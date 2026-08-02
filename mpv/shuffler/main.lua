26b62ccafbe7c3a5111ce77312ea384a6db1d62c
kufern <kufern@disroot.org>
2026-08-02 22:40:58 +0700

    man lua is hard

--- mpv/shuffler/main.lua
+++ mpv/shuffler/main.lua
@@ -1,11 +1,20 @@
-local utils = require("mp.utils") -- mpv helper functions
+utils = require("mp.utils") -- mpv helper functions
+file_path = mp.get_script_directory() .. "/dir.txt"
 
 -- add directories
 
-script_dir = mp.get_script_directory()
-dir = nil
+local existing_dir = {}
+local file_read = io.open(file_path, "r")
+if file_read then
+	for line in file_read:lines() do
+		local clean_line = line:gsub("^%s*(.-)%s*$", "")
+		if clean_line ~= "" then
+			existing_dir[clean_line] = true
+		end
+	end
+	file_read:close()
+end
 
-file = io.open(script_dir .. "/dir.txt", "a+")
 while true do
 	io.write("Add new directory: ")
 	dir = io.read()
@@ -13,17 +22,28 @@ while true do
 	if dir == "" then
 		print("No data written")
 	elseif dir == "exit" then
-		file:close()
 		print("Completed adding directories.")
 		break
 	else
-		dir_metadata = utils.file_info(dir)
-		if dir_metadata and dir_metadata.is_directory then
-			file:write(dir .. "\n")
-			file:flush()
-		elseif not dir_metadata then
+		local clean_dir = dir:gsub("^%s*(.-)%s*$", "")
+		local metadata = utils.file_info(clean_dir)
+		if metadata and metadata.is_directory then
+			if existing_dir[clean_dir] then
+				print("Directory is already here.")
+			else
+				local file_append = io.open(file_path, "a")
+				if file_append then
+					file_append:write(clean_dir .. "\n")
+					file_append:close()
+					existing_dir[clean_dir] = true
+					print("Directory added.")
+				else
+					print("Failed to open file.")
+				end
+			end
+		elseif not metadata then
 			print("Directory does not exist.")
-		elseif not dir_metadata.is_directory then
+		elseif not metadata.is_directory then
 			print("This is not a directory.")
 		end
 	end
