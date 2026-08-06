7508cd51cd2c2e557e536189fe3373ac64428f76
kufern <kufern@disroot.org>
2026-08-06 21:25:24 +0700

    add files

--- mpv/shuffler/main.lua
+++ mpv/shuffler/main.lua
@@ -3,7 +3,7 @@ file_path = mp.get_script_directory() .. "/dir.txt"
 
 -- add directories
 
-local existing_dir = {}
+existing_dir = {}
 local file_read = io.open(file_path, "r")
 if file_read then
 	for line in file_read:lines() do
@@ -15,44 +15,73 @@ if file_read then
 	file_read:close()
 end
 
-while true do
-	io.write("Add new directory: ")
-	local dir = io.read()
+io.write("Add new directory?")
+local ans = io.read()
 
-	if dir == "" then
-		print("No data written")
-	elseif dir == "exit" then
-		print("Completed adding directories.")
-		break
-	else
-		local clean_dir = dir:gsub("^%s*(.-)%s*$", "")
-		local metadata = utils.file_info(clean_dir)
-		if metadata and metadata.is_directory then
-			if existing_dir[clean_dir] then
-				print("Directory is already here.")
-			else
-				local file_append = io.open(file_path, "a")
-				if file_append then
-					file_append:write(clean_dir .. "\n")
-					file_append:close()
-					existing_dir[clean_dir] = true
-					print("Directory added.")
+if ans:lower() == "y" then
+	while true do
+		io.write("New directory: ")
+		local dir = io.read()
+
+		if dir == "" then
+			print("No data written")
+		elseif dir == "exit" then
+			print("Completed adding directories.")
+			break
+		else
+			local clean_dir = dir:gsub("^%s*(.-)%s*$", "")
+			local metadata = utils.file_info(clean_dir)
+			if metadata and metadata.is_directory then
+				if existing_dir[clean_dir] then
+					print("Directory is already here.")
 				else
-					print("Failed to open file.")
+					local file_append = io.open(file_path, "a")
+					if file_append then
+						file_append:write(clean_dir .. "\n")
+						file_append:close()
+						existing_dir[clean_dir] = true
+						print("Directory added.")
+					else
+						print("Failed to open file.")
+					end
+				end
+			elseif not metadata then
+				print("Directory does not exist.")
+			elseif not metadata.is_directory then
+				print("This is not a directory.")
+			end
+		end
+	end
+else
+	print("Preparing to shuffle...")
+end
+
+-- find all music files
+
+all_files = {}
+audio_extensions = {
+	aiff = true, alac = true, flac = true, m4a = true, mp1 = true, mp2 = true, mp3 = true, oga = true, ogg = true, mogg = true, opus = true,
+	qoa = true, raw = true, wav = true, wma = true
+}
+
+function recursive_files(dir)
+	local all_files_in_dir = utils.readdir(dir, "files") or {}
+	for _, name in ipairs(all_files_in_dir) do
+		if name ~= "." and name ~= ".." then
+			local entire_path = dir .. "/" .. name
+			local info = utils.file_info(entire_path)
+			if info and info.is_directory then
+				table.insert(all_files, file)
+			else
+				local extension = name:match("%.([^%.]+)$")
+				if extension and audio_extensions[extension:lower()] then
+					table.insert(all_files, entire_path)
 				end
 			end
-		elseif not metadata then
-			print("Directory does not exist.")
-		elseif not metadata.is_directory then
-			print("This is not a directory.")
 		end
 	end
 end
 
--- shuffler
-
-local file_shuffle = io.open(file_path, "r")
-if file_shuffle then
-	for line in file_shuffle:lines() do
-	end
+for dir in pairs(existing_dir) do
+	recursive_files(dir)
 end
